5cacdf17e91bd9120f059c9da868a1b520536bab
Curtion <curtion@126.com>
2023-02-15 23:18:56 +0800

    feat: 可行性验证成功(签到部分)

---
+++ background.js
@@ -0,0 +1,19 @@
+chrome.alarms.create("jobs", { when: Date.now(), periodInMinutes: 100 });
+chrome.alarms.onAlarm.addListener(() => {
+  console.log('ok')
+  sign()
+});
+
+function sign() {
+  const url = 'https://www.hdarea.co/sign_in.php'
+  const data = new FormData()
+  data.append('action', 'sign_in')
+  const options = {
+    method: 'POST',
+    credentials: 'include',
+    body: data
+  }
+  fetch(url, options).then(res => {
+    console.log(res)
+  })
+}
--- popup.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  console.log(chrome.tabs)
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    console.log(activeTab.url);
-  });
-});
